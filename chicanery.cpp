b7b1b7f83f97e447247a54c5efdbdd5f1bfdd028
Jon Cili <j.c.master775@gmail.com>
2023-06-22 15:48:59 -0400

    first commit

---
+++ chicanery.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::to_string;
+using std::stoi;
+
+int main () {
+    cout << "Welcome to Raising Chicanery! Watch our dog do some cool tricks: " << '\n';
+}
