ff6ca020cd68d32378117f064517dafe603bbee2
Jon Cili <j.c.master775@gmail.com>
2023-06-22 16:31:39 -0400

    funadmentals for any sfml project

--- chicanery.cpp
+++ chicanery.cpp
@@ -10,7 +10,28 @@ using std::string;
 using std::to_string;
 using std::stoi;
 
+#include <SFML/Graphics.hpp>
+
 int main () {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
     cout << "VS PetCode is awesome" << "\n";
     cout << "Welcome to Raising Chicanery! Watch our dog do some cool tricks: " << '\n';
 }
